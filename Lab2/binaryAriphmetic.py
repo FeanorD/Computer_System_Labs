90da15f475423205b9b8bbfe96adb51a31bd6374
FeanorD <meleshkodim2000@gmail.com>
2020-02-25 13:57:12 +0200

    add binary division

--- Lab2/binaryAriphmetic.py
+++ Lab2/binaryAriphmetic.py
@@ -27,8 +27,50 @@ if __name__ == "__main__":
         print(f"\n\t\t+Final sum:   {sum}")
         return sum
 
+    def binarySubtraction(a, b):
+        print("\n\t\tSUBTRACTION:\n")
+        diff = len(a)-len(b)
+
+        if (diff > 0):
+            b = ([0,] * diff) + b   
+        elif (diff < 0):
+            a = ([0,] * (-diff)) + a 
+
+        print(f"\t\tTerm 'a': {a}",
+              f"\n\t\tTerm 'b': {b}\n")
+
+        sign = 0
+        subtr = [0,] * len(a)
+        carry = 0
+
+        for i in reversed(range(len(a))):
+            d = a[i] - b[i]
+            # print(d)
+            if d <= 0:
+                if (carry == 0 and d != 0):
+                    carry = 1
+                    subtr[i] = 1
+                elif (carry == 1):
+                    subtr[i] = 0
+                else:
+                    subtr[i] = 0
+            else:
+                if (carry == 1):
+                    carry = 0
+                    subtr[i] = 0
+                else:
+                    subtr[i] = 1
+
+        if (carry == 1):
+            sign = 1                        
+
+        return subtr, sign
+
     def shiftRight(reg):
         return [0,] + reg
+
+    def shiftLeft(reg, value = 0):
+        return reg + [value]
 
     def binaryMultiplication(multiplicand, multiplier):
         print("MULTIPLICATION:\n")
@@ -61,5 +103,45 @@ if __name__ == "__main__":
 
         return register
 
-    res = binaryMultiplication([1,1,1,1], [1,1,1,1])
-    print(f"\n=RESULT: {res}\n")
+    def binaryDivision(dividend, divisor):
+        remainder = [0,] * 16
+        quotient = []
+        halfRegisterSize = int(len(remainder) / 2)
+        print(halfRegisterSize)
+        remainder = binaryAddition(remainder, dividend)
+        print(f"initial remainder value: {remainder}")
+        tempRemainder = []
+
+        for i in range(halfRegisterSize):
+            remainder = [remainder[j] for j in reversed(range(len(remainder)))]
+            print(f"remainder after first inversion: {remainder}")
+            remainder.pop()
+            remainder = [remainder[j] for j in reversed(range(len(remainder)))]
+            print(f"remainder after SECOND inversion: {remainder}")
+            
+            remainder = shiftLeft(remainder)
+            tempRemainder = remainder[:]
+            print(f"remainder: {remainder} and [:half of Register Size] {remainder[:halfRegisterSize]}")
+            subtractResult, sign = binarySubtraction(remainder[:halfRegisterSize], divisor)
+
+            remainder = subtractResult + remainder[halfRegisterSize:]
+
+            print(f"sign: {sign}")
+            
+            if sign == 1:
+                remainder = tempRemainder[:]
+                quotient.append(0)
+            else:
+                quotient.append(1)
+            print(f"QUOTIENT: {quotient}")
+                                
+        return quotient, remainder[:halfRegisterSize]
+
+    # multiplicationRes = binaryMultiplication([1,1,1,1], [1,1,1,1])
+    # print(f"\n=RESULT: {multiplicationRes}\n")
+    # subtract, sign = binarySubtraction([1,1,0,0], [0,1])
+    # print(subtract, f"\n Znak: {sign}")
+
+    # test, rem = binaryDivision([0,1,0,0,1,0,0,0], [1,0,0,0,0])
+    test, rem = binaryDivision([0,1,0,0,0,0,1,0], [1,1,0])
+    print(f"RESULT: {test}", f"REMAINDER: {rem}")
