f94349471a847dbb1bcec1b6653047a3a10e1368
FeanorD <meleshkodim2000@gmail.com>
2020-02-26 18:44:26 +0200

    add float binary multiplication

--- Lab2/binaryAriphmetic.py
+++ Lab2/binaryAriphmetic.py
@@ -73,52 +73,50 @@ def shiftLeft(reg, value = 0):
 
 def binaryMultiplication(multiplicand, multiplier):
         print("MULTIPLICATION:\n")
-        register = [0,] * 48
+        register = [0,] * 8
         halfRegisterSize = int(len(register) / 2)
         multiplicand = [0,] * (halfRegisterSize - len(multiplicand)) + multiplicand
         multiplier = [0,] * (halfRegisterSize - len(multiplier)) + multiplier
         print(f'\tMultiplicand: {multiplicand}',
               f'\n\tMultiplier:   {multiplier}\n')
-        # print(f"\t*Register: {register}")
-        # print(f"\t+Add multiplier to register")
+        print(f"\t*Register: {register}")
+        print(f"\t+Add multiplier to register")
         register = binaryAddition(register, multiplier)
 
         iteration = 0
 
         while (iteration <= len(multiplicand)):
             currentBit = register.pop()
-            # print("\t*Shift register right")
-            # print(f"\tPop bit: {currentBit}")
+            print("\t*Shift register right")
+            print(f"\tPop bit: {currentBit}")
             register = shiftRight(register)
-            # print(f"\t*Register: {register}")
+            print(f"\t*Register: {register}")
 
             if (currentBit == 1):
-                # print("\t+Add multiplicand to first half of register")
+                print("\t+Add multiplicand to first half of register")
                 register = binaryAddition(register[:halfRegisterSize], multiplicand) + register[halfRegisterSize:]
-                # print(f"\t*Register: {register}")
+                print(f"\t*Register: {register}")
 
             iteration += 1
 
         return register
 
 def binaryDivision(dividend, divisor):
-        remainder = [0,] * 16
+        print("DIVISION")
+        remainder = [0,] * 8
         quotient = []
         halfRegisterSize = int(len(remainder) / 2)
+        print("Initialize remainder by adding DIVIDEND to it")
         remainder = binaryAddition(remainder, dividend)
         print(f"initial remainder value: {remainder}")
         tempRemainder = []
 
         for i in range(halfRegisterSize):
-            # remainder = [remainder[j] for j in reversed(range(len(remainder)))]
-            print(f"remainder after first inversion: {remainder}")
-            remainder.pop(0)
-            # remainder = [remainder[j] for j in reversed(range(len(remainder)))]
-            print(f"remainder after SECOND inversion: {remainder}")
-            
+            print(f"Shift remaider left: {remainder} <--")
+            remainder.pop(0)         
             remainder = shiftLeft(remainder)
             tempRemainder = remainder[:]
-            print(f"remainder: {remainder} and [:half of Register Size] {remainder[:halfRegisterSize]}")
+            print(f"After shift remainder: {remainder}")
             subtractResult, sign = binarySubtraction(remainder[:halfRegisterSize], divisor)
 
             remainder = subtractResult + remainder[halfRegisterSize:]
@@ -134,14 +132,9 @@ def binaryDivision(dividend, divisor):
                                 
         return quotient, remainder[:halfRegisterSize]
 
-    # multiplicationRes = binaryMultiplication([1,1,1,1], [1,1,1,1])
-    # print(f"\n=RESULT: {multiplicationRes}\n")
-    # subtract, sign = binarySubtraction([1,1,0,0], [0,1])
-    # print(subtract, f"\n Znak: {sign}")
-
-    # test, rem = binaryDivision([0,1,0,0,1,0,0,0], [1,0,0,0,0])
 if __name__ == "__main__":
-        # test, rem = binaryDivision([0,1,0,0,1,0,0,0], [1,0,0,0])
-        # print(f"RESULT: {test}", f"REMAINDER: {rem}")
-        subtract, sign = binarySubtraction([1,0,0,0,0,0,1,1,1], [0,1,1,1,1,1,1,0])
-        print(subtract)
+        test, rem = binaryDivision([0,1,0,0,1,0,0,0], [1,0,0,0])
+        print(f"RESULT: {test}", f"REMAINDER: {rem}")
+
+        # multiplicationRes = binaryMultiplication([1,1,1,1], [1,1,1,1])
+        # print(f"\n=RESULT: {multiplicationRes}\n")
